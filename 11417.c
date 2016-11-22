b1de26683e265601ed635e80b8d1b997ddd2845b
Shams <shamsiitsecond@gmail.com>
2016-11-22 17:55:55 +0600

    Add files via upload

---
+++ 11417.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int GCD(int a,int b)
+{
+    int c;
+    while(a!=0)
+    {
+        c=b%a;
+        b=a;
+        a=c;
+    }
+    return(b);
+}
+int main()
+{
+    int a,b,N;
+    while(scanf("%d",&N) && N!=0)
+    {
+        long G=0;
+        for(a=1;a<N;a++){
+            for(b=a+1;b<=N;b++)
+            {
+                G+=GCD(a,b);
+            }
+        }
+        
+        printf("%ld\n",G);
+    }
+    return(0);
+}
